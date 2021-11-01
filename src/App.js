5d80b1d78c33c5d0f32e4829ed17fe888cb1d733
Osama <osamanadeem679@gmail.com>
2021-11-01 13:10:57 +0500

    styled Tinder Cards component, setup firebase storage and created people collection, fetched people from firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './Header/Header';
+import TinderCards from './TinderCards/TinderCards';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Router>
+        <Switch>
+          <Route path="/chat" ></Route>
+          <Route path="/" ><TinderCards /></Route>
+        </Switch>
+      </Router>
       {/* Tinder Cards */}
       {/* Buttons below Tinder cards */}
       {/* Chat Screen */}
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDDopcS_agSHEJfb2m5LESHHPoZaujiupg",
@@ -8,3 +9,8 @@ const firebaseConfig = {
     appId: "1:686706108802:web:ae04cb9e499d87ff06079b",
     measurementId: "G-4BPWPXWLTH"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+export default db;
